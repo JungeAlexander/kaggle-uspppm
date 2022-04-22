f0a63eb517221b5e74d3ba40f10820fac3a5b7ff
Alexander Junge <alexander.junge@gmail.com>
2022-04-22 07:54:11 +0200

    Set input and output

--- src/gradio_app.py
+++ src/gradio_app.py
@@ -1,13 +1,16 @@
 import gradio as gr
 
 
-def greet(name):
-    return f"Hello {name}!!"
+def greet(anchor, target):
+    return len(anchor + target)
 
 
-inputs = gr.inputs.Textbox(lines=2, placeholder="Name Here...")
+anchor_input = gr.inputs.Textbox(lines=1, placeholder="Anchor")
+target_input = gr.inputs.Textbox(lines=1, placeholder="Target")
 
-iface = gr.Interface(fn=greet, inputs=inputs, outputs="text")
+sim_output = gr.outputs.Textbox(type="number", label="Similarity")
+
+iface = gr.Interface(fn=greet, inputs=[anchor_input, target_input], outputs=sim_output)
 
 if __name__ == "__main__":
     app, local_url, share_url = iface.launch()
